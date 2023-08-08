45cf1b6faecb5c1b90f51879e4b68fc5ba15ae3f
hchoi3 <hyunmin.choi@axleinfo.com>
2023-08-08 09:16:33 -0400

    added output and new varaible name

--- terraform-modules/aws/platform-services/iam_roles/flow_log_role/variables.tf
+++ terraform-modules/aws/platform-services/iam_roles/flow_log_role/variables.tf
@@ -1,9 +1,9 @@
 variable "iam_role_name" {
   type    = string
-  default = "flow_log_role"
+  default = "flowlog_role"
 }
 
 variable "iam_role_policy_name" {
   type    = string
-  default = "flow_log_role_policy"
+  default = "flowlog_role_policy"
 }
