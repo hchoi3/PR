04e93e8089459626c3628d8ede7f9e06c47c1cb1
hchoi3 <hyunmin.choi@axleinfo.com>
2023-08-09 10:28:07 -0400

    cleaned up repo for cloud watch alarm

--- terraform-modules/aws/platform-services/iam_roles/flow_log_role/output.tf
+++
@@ -1,3 +0,0 @@
-output "flow_log_role_arn" {
-  value = aws_iam_role.flow_log_role.arn
-}
--- terraform-modules/aws/platform-services/iam_roles/flow_log_role/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "iam_role_name" {
-  type    = string
-  default = "flowlog_role"
-}
-
-variable "iam_role_policy_name" {
-  type    = string
-  default = "flowlog_role_policy"
-}
