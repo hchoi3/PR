4cebd154dfd698dc96a2eef9e3cb6a9d5903e5ad
hchoi3 <hyunmin.choi@axleinfo.com>
2024-02-01 15:42:20 -0500

    check branch

--- provider.tf
+++ provider.tf
@@ -2,7 +2,5 @@ provider "aws" {
   version = "~> 4.0"
   region  = var.AWS_REGION
   shared_credentials_files = "./aws/credentials"
-  assume_role {
-    role_arn    = var.IAM_role_arn
-  }
+  profile = "aws"
 }  
