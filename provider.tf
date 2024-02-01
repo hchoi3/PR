1dd8504fa6b6816bb27830b77e65964e682c50e0
hchoi3 <hyunmin.choi@axleinfo.com>
2024-02-01 15:38:51 -0500

    added

--- provider.tf
+++ provider.tf
@@ -1,8 +1,7 @@
 provider "aws" {
   version = "~> 4.0"
   region  = var.AWS_REGION
-  access_key = "AWS_ACCESS_KEY"
-  secret_key = "AWS_SECRET_ACCESS_KEY"
+  shared_credentials_files = "./aws/credentials"
   assume_role {
     role_arn    = var.IAM_role_arn
   }
