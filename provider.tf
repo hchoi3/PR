5b61ff672a8f5ee6cee6985dd2848f46fee2cbdc
hchoi3 <hyunmin.choi@axleinfo.com>
2024-02-01 15:49:13 -0500

    added vul

--- provider.tf
+++ provider.tf
@@ -1,6 +1,14 @@
+# Terraform version 1.2.3
+terraform {
+  required_version = "=1.2.4"
+}
+
 provider "aws" {
   version = "~> 4.0"
   region  = var.AWS_REGION
-  shared_credentials_files = "./aws/credentials"
-  profile = "aws"
-}  
+  access_key = "AWS_ACCESS_KEY"
+  secret_key = "AWS_SECRET_ACCESS_KEY"
+  assume_role {
+    role_arn    = var.IAM_role_arn
+  }
+}
